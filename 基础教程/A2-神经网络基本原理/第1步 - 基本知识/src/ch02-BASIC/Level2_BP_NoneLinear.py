3d11326bbab435a6c1bbb9a5c99ef0d352dba6d6
Bohao Li <bohao@fb.com>
2023-03-09 11:51:59 -0800

    Cleaned up pytorch neural net linear regression, with help from chat gpt.

--- 基础教程/A2-神经网络基本原理/第1步 - 基本知识/src/ch02-BASIC/Level2_BP_NoneLinear.py
+++ 基础教程/A2-神经网络基本原理/第1步 - 基本知识/src/ch02-BASIC/Level2_BP_NoneLinear.py
@@ -1,44 +1,55 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+# Import necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Define function to draw a plot of the given function and input/output points
 def draw_fun(X,Y):
+    # Define x values to plot the function
     x = np.linspace(1.2,10)
+    # Calculate intermediate variables a, b, and c based on x values
     a = x*x
     b = np.log(a)
     c = np.sqrt(b)
+    # Plot the function
     plt.plot(x,c)
-
+    # Plot the input/output points
     plt.plot(X,Y,'x')
-
+    # Calculate and plot the derivative of the function
     d = 1/(x*np.sqrt(np.log(x**2)))
     plt.plot(x,d)
+    # Show the plot
     plt.show()
 
-
+# Define forward function that calculates intermediate variables a, b, and c based on x
 def forward(x):
     a = x*x
     b = np.log(a)
     c = np.sqrt(b)
     return a,b,c
 
+# Define backward function that calculates the loss and gradients based on intermediate variables and y
 def backward(x,a,b,c,y):
+    # Calculate the loss between c and y
     loss = c - y
+    # Calculate gradients using the chain rule
     delta_c = loss
     delta_b = delta_c * 2 * np.sqrt(b)
     delta_a = delta_b * a
     delta_x = delta_a / 2 / x
     return loss, delta_x, delta_a, delta_b, delta_c
 
+# Define update function that updates x based on the calculated gradient and a lower bound of 1.1
 def update(x, delta_x):
     x = x - delta_x
     if x < 1:
         x = 1.1
     return x
 
+# Main function
 if __name__ == '__main__':
+    # Prompt user to input initial value x and target value y
     print("how to play: 1) input x, 2) calculate c, 3) input target number but not faraway from c")
     print("input x as initial number(1.2,10), you can try 1.3:")
     line = input()
@@ -50,29 +61,31 @@ if __name__ == '__main__':
     line = input()
     y = float(line)
 
+    # Set error tolerance for convergence
     error = 1e-3
 
+    # Initialize lists to store x and c values for plotting
     X,Y = [],[]
 
+    # Loop for a maximum of 20 iterations
     for i in range(20):
-        # forward
+        # Forward pass to calculate intermediate variables and c
         print("forward...")
         a,b,c = forward(x)
         print("x=%f,a=%f,b=%f,c=%f" %(x,a,b,c))
+        # Store x and c values for plotting
         X.append(x)
         Y.append(c)
-        # backward
+        # Backward pass to calculate gradients
         print("backward...")
         loss, delta_x, delta_a, delta_b, delta_c = backward(x,a,b,c,y)
+        # Check for convergence
         if abs(loss) < error:
             print("done!")
             break
-        # update x
+        # Update x based on gradients
         x = update(x, delta_x)
         print("delta_c=%f, delta_b=%f, delta_a=%f, delta_x=%f\n" %(delta_c, delta_b, delta_a, delta_x))
 
-    
-    draw_fun(X,Y)
-
-
-
+# Draw plot of function and input/output points
+draw_fun(X,Y)
--- 基础教程/A2-神经网络基本原理/第2步 - 线性回归/src/ch04-SingleVariableLinearRegression/Level7_Pytorch.py
+++ 基础教程/A2-神经网络基本原理/第2步 - 线性回归/src/ch04-SingleVariableLinearRegression/Level7_Pytorch.py
@@ -1,66 +1,87 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from pathlib import Path
-import math
-from torch.utils.data import TensorDataset, DataLoader
+# Import necessary libraries
 from HelperClass.DataReader_1_0 import *
-import torch.nn as nn
-import torch.nn.functional as F
-import torch
 from torch.optim import Adam
-import warnings
-warnings.filterwarnings('ignore')
+from torch.utils.data import TensorDataset, DataLoader
 
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+
+# Set filename for data
 file_name = "ch04.npz"
+
+# Define neural network model class
 
 
 class Model(nn.Module):
     def __init__(self, input_size):
         super(Model, self).__init__()
         self.fc = nn.Linear(input_size, 1)
+
     def forward(self, x):
         x = self.fc(x)
         return x
 
+
+# Main function
 if __name__ == '__main__':
+    # Set maximum number of epochs to train for
     max_epoch = 500
-    num_category = 3
+    # Create data reader object and load data from file
     sdr = DataReader_1_0(file_name)
     sdr.ReadData()
 
-    num_input = 1       # input size
-    # get numpy form data
+    # Set input size
+    num_input = 1
+    # Get input and output data in numpy array form
     XTrain, YTrain = sdr.XTrain, sdr.YTrain
-    torch_dataset = TensorDataset(torch.FloatTensor(XTrain), torch.FloatTensor(YTrain))
+    # Create PyTorch dataset from numpy arrays
+    torch_dataset = TensorDataset(
+        torch.FloatTensor(XTrain), torch.FloatTensor(YTrain))
 
-    train_loader = DataLoader(          # data loader class
+    # Create PyTorch data loader from dataset
+    train_loader = DataLoader(
         dataset=torch_dataset,
         batch_size=32,
         shuffle=True,
     )
 
+    # Use Mean Squared Error loss function because we are doing regression and not classification.
+    # If we were doing classification, we would use Cross Entropy loss function.
     loss_func = nn.MSELoss()
     model = Model(num_input)
+    # Define optimizer for model parameters
     optimizer = Adam(model.parameters(), lr=1e-2)
 
-    e_loss = []     # mean loss at every epoch
+    # Initialize list to store mean loss at every epoch
+    e_loss = []
+    # Loop through each epoch
     for epoch in range(max_epoch):
-        b_loss = []     # mean loss at every batch
+        # Initialize list to store mean loss at every batch
+        b_loss = []
+        # Loop through each batch in the data loader
         for step, (batch_x, batch_y) in enumerate(train_loader):
+            # Zero the gradients of the model parameters
             optimizer.zero_grad()
+            # Forward pass through the model to get predictions
             pred = model(batch_x)
-            loss = loss_func(pred,batch_y)
+            # Calculate loss between predictions and true values
+            loss = loss_func(pred, batch_y)
+            # Store loss value for the batch
             b_loss.append(loss.cpu().data.numpy())
+            # Backward pass to calculate gradients
             loss.backward()
+            # Update model parameters using optimizer
             optimizer.step()
             b_loss.append(loss.cpu().data.numpy())
+        # Store mean loss for the epoch
         e_loss.append(np.mean(b_loss))
+        # Print mean loss for the epoch every 20 epochs
         if epoch % 20 == 0:
             print("Epoch: %d, Loss: %.5f" % (epoch, np.mean(b_loss)))
+    # Plot mean loss versus epoch
     plt.plot([i for i in range(max_epoch)], e_loss)
     plt.xlabel('Epoch')
     plt.ylabel('Mean loss')
     plt.show()
-
-
-
